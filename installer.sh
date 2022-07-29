afe536b55123406d18bf6ec1369edc9582288cc5
KGVarunkanth <kgvarunkanth@gmail.com>
2022-07-29 21:30:47 +0530

    Add installer.sh script for easy installation

---
+++ installer.sh
@@ -0,0 +1,8 @@
+pkg update & pkg upgrade -y
+apt install python3 nano wget -y
+python3 -m pip install --upgrade pip
+pip install tweepy
+wget "https://raw.githubusercontent.com/kgvarunkanth/twitter_tweepy_autobot/main/config.py"
+wget "https://raw.githubusercontent.com/kgvarunkanth/twitter_tweepy_autobot/main/credentials.py"
+wget "https://raw.githubusercontent.com/kgvarunkanth/twitter_tweepy_autobot/main/sample.py"
+
