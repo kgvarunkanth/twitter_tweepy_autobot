4d381eec1461d6ea1f027a2f6ac880322731ba71
KGVarunkanth <kgvarunkanth@gmail.com>
2022-07-30 15:07:59 +0530

    Update installer script
    1. Fix a typo
    2. Switch to pkg (Package manager of termux)

--- installer.sh
+++ installer.sh
@@ -1,5 +1,5 @@
-pkg update & pkg upgrade -y
-apt install python3 nano wget -y
+pkg update -y && pkg upgrade -y
+pkg install python3 nano wget -y
 python3 -m pip install --upgrade pip
 pip install tweepy
 wget "https://raw.githubusercontent.com/kgvarunkanth/twitter_tweepy_autobot/main/config.py"
